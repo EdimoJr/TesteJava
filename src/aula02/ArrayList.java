749f0a7f9c9e7615a437f3a312950c8d9bae3624
Edimo (Divino) Silva Junior <esilvaju@emeal.nttdata.com>
2026-04-16 14:52:40 -0300

    correcão no exemplo ArrayList

--- src/aula02/ArrayList.java
+++
@@ -1,8 +0,0 @@
-package aula02;
-
-public class ArrayList {
-    public static void main(String[] args){
-        //ArrayList<String> nomes = new ArrayList<>();
-
-    }
-}
---
+++ src/aula02/ArrayListExemplo.java
@@ -0,0 +1,14 @@
+package aula02;
+
+import java.util.ArrayList;
+
+public class ArrayListExemplo {
+    public static void main(String[] args){
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Edimo");
+        nomes.add("Kleber");
+        nomes.add("Olesio");
+        nomes.add("Dantas");
+        System.out.println(nomes);
+    }
+}
